feb3242f2f1f6443314f919dc55a499600a4142b
Mateus Marinho <mateus.marinho@hotmail.com.br>
2022-01-21 14:24:05 -0300

    add todo

--- frontend/helpers/apimock/db_todos.js
+++ frontend/helpers/apimock/db_todos.js
@@ -1,6 +1,7 @@
 export const todos = {
   todos: [
     {description: 'Do the laundry', done: true},
-    {description: 'Walk the dog', done: false}
+    {description: 'Walk the dog', done: false},
+    {description: 'Teste', done: false}
   ]
 }
